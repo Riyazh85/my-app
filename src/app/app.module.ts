19d53ec9b92db283b20ec92eae9b9fe7dec73c86
Riyaz Hawaldar <riyaz@Riyazs-MacBook-Air.local>
2019-07-28 20:47:46 +0530

    base version of app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { EnquiryComponent } from './enquiry/enquiry.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    EnquiryComponent
   ],
   imports: [
     BrowserModule,
